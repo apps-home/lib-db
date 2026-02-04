816c6198706df63d06502903f9f84acedc16348c
Lucasdias067 <lucasdias067@gmail.com>
2026-02-03 21:06:34 -0300

    commit

--- src/client/index.ts
+++ src/client/index.ts
@@ -6,16 +6,11 @@ export function getDriver(): PrismaNeon {
 	if (!_driver) {
 		const connectionString = process.env.DATABASE_URL
 
-		if (!connectionString) {
-			throw new Error('DATABASE_URL environment variable is not set')
-		}
-
 		_driver = new PrismaNeon({ connectionString })
 	}
 	return _driver
 }
 
-// For backwards compatibility
 export const driver = new Proxy({} as PrismaNeon, {
 	get(_target, prop) {
 		return (getDriver() as any)[prop]
