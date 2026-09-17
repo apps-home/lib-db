6966cd4e5c6256d110a2d7c03dbb469b5e674242
Lucasdias067 <lucasdias067@gmail.com>
2026-09-17 09:46:51 -0300

    feat: switch from PrismaNeon to PrismaPg adapter for database connection

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,17 +1,19 @@
-import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-let _driver: PrismaNeon | null = null
+let _driver: PrismaPg | null = null
 
-export function getDriver(): PrismaNeon {
+export function getDriver(): PrismaPg {
   if (!_driver) {
     const connectionString = process.env.DATABASE_URL
+    const pool = new Pool({ connectionString })
 
-    _driver = new PrismaNeon({ connectionString })
+    _driver = new PrismaPg(pool)
   }
   return _driver
 }
 
-export const driver = new Proxy({} as PrismaNeon, {
+export const driver = new Proxy({} as PrismaPg, {
   get(_target, prop) {
     return (getDriver() as any)[prop]
   }
